dcf6dbefac02993c5ce9c10d5c14ae2c38b968d4
cookiemouse <cookiemouse@163.com>
2017-08-29 09:34:59 +0800

    RegularU添加判断字符串是否有内容

--- RegularU.java
+++ RegularU.java
@@ -1,5 +1,7 @@
 package com.tianyigps.dispatch2.utils;
 
+import android.util.Log;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,9 +10,18 @@ import java.util.regex.Pattern;
  */
 
 public class RegularU {
+
+    private static final String TAG = "RegularU";
+
     public static boolean checkCarNo(String carNo) {
-        Pattern pattern = Pattern.compile("/^[\\u4e00-\\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5,6}$/");
+        Log.i(TAG, "checkCarNo: carNo-->" + carNo);
+        Pattern pattern = Pattern.compile("[\\u4e00-\\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5,6}$");
         Matcher matcher = pattern.matcher(carNo);
+        Log.i(TAG, "checkCarNo: result-->" + matcher.matches());
         return matcher.matches();
+    }
+
+    public static boolean isNull(String content) {
+        return null == content || "".equals(content);
     }
 }
