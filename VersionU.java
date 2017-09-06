eeaf1b83e11f7d0b776267c5968f44957eb22095
cookiemouse <cookiemouse@163.com>
2017-09-06 14:40:27 +0800

    添加获取本地apk versionCode与versionName工具

---
+++ VersionU.java
@@ -0,0 +1,50 @@
+package com.tianyigps.online.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by cookiemouse on 2017/9/6.
+ */
+
+public class VersionU {
+
+    /**
+     * get App versionCode
+     *
+     * @param context
+     * @return
+     */
+    public String getVersionCode(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionCode = "";
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = packageInfo.versionCode + "";
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    /**
+     * get App versionName
+     *
+     * @param context
+     * @return
+     */
+    public static String getVersionName(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionName = "";
+        try {
+            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionName;
+    }
+}
