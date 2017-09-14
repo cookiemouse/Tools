58d2b49bc94424fbd53d2716cb1f6482d196373b
cookiemouse <cookiemouse@163.com>
2017-09-14 09:36:15 +0800

    正则表达式，匹配网址

--- RegularU.java
+++ RegularU.java
@@ -12,16 +12,25 @@ import java.util.regex.Pattern;
 public class RegularU {
 
     private static final String TAG = "RegularU";
+    private static final String CHECK_CAR_NO = "[\\u4e00-\\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5,6}$";
+    private static final String CHECK_WEB_ADDRESS = "^([hH][tT]{2}[pP]://|[hH][tT]{2}[pP][sS]://)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~\\/])+$";
 
     public static boolean checkCarNo(String carNo) {
         Log.i(TAG, "checkCarNo: carNo-->" + carNo);
-        Pattern pattern = Pattern.compile("[\\u4e00-\\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5,6}$");
+        Pattern pattern = Pattern.compile(CHECK_CAR_NO);
         Matcher matcher = pattern.matcher(carNo);
         Log.i(TAG, "checkCarNo: result-->" + matcher.matches());
         return matcher.matches();
     }
 
-    public static boolean isNull(String content) {
+    public static boolean isEmpty(String content) {
         return null == content || "".equals(content);
+    }
+
+    //  是否为网址
+    public static boolean isWebAddress(String address) {
+        Pattern pattern = Pattern.compile(CHECK_WEB_ADDRESS);
+        Matcher matcher = pattern.matcher(address);
+        return matcher.matches();
     }
 }
