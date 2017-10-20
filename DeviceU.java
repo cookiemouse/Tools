21f990018b8bbb946f99a93edb007b629220cc70
cookiemouse <cookiemouse@163.com>
2017-10-20 16:31:45 +0800

    将VersionU改为DeviceU，并添加获取设备信息的方法

--- DeviceU.java
+++ DeviceU.java
@@ -3,12 +3,13 @@ package com.tianyigps.online.utils;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.telephony.TelephonyManager;
 
 /**
  * Created by cookiemouse on 2017/9/6.
  */
 
-public class VersionU {
+public class DeviceU {
 
     /**
      * get App versionCode
@@ -46,5 +47,57 @@ public class VersionU {
             e.printStackTrace();
         }
         return versionName;
+    }
+
+    /**
+     * 获取设备的唯一标识，deviceId
+     *
+     * @param context
+     * @return
+     */
+    public static String getDeviceId(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String deviceId = tm.getDeviceId();
+        if (deviceId == null) {
+            return "-";
+        } else {
+            return deviceId;
+        }
+    }
+
+    /**
+     * 获取手机品牌
+     *
+     * @return
+     */
+    public static String getPhoneBrand() {
+        return android.os.Build.BRAND;
+    }
+
+    /**
+     * 获取手机型号
+     *
+     * @return
+     */
+    public static String getPhoneModel() {
+        return android.os.Build.MODEL;
+    }
+
+    /**
+     * 获取手机Android API等级（22、23 ...）
+     *
+     * @return
+     */
+    public static int getBuildLevel() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * 获取手机Android 版本（4.4、5.0、5.1 ...）
+     *
+     * @return
+     */
+    public static String getBuildVersion() {
+        return android.os.Build.VERSION.RELEASE;
     }
 }
