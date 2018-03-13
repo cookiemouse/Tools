cbaead5b9d379769752d85f6f79c4bcb2621a69a
cookiemouse <cookiemouse@163.com>
2018-03-13 10:05:28 +0800

    添加判断文本里是否含有Emoji

---
+++ hadEmoji.java
@@ -0,0 +1,20 @@
+//  验证是否含有表情
+    private boolean hadEmoje(String str) {
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (isEmojiCharacter(str.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isEmojiCharacter(char codePoint) {
+        return !((codePoint == 0x0) ||
+                (codePoint == 0x9) ||
+                (codePoint == 0xA) ||
+                (codePoint == 0xD) ||
+                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
+                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
+                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF)));
+    }
